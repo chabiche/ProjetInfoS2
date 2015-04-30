1103e9916dc4de4dc8dae4a56821a4eadb4ef761
chabiche <chabiche@ensc.fr>
2015-04-30 23:57:12 +0200

    ghjbn

---
+++ ProjetInfoS2/ProjetInfoS2/Cuisinier.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjetInfoS2
+{
+    class Cuisinier
+    {
+        //Variables d'instance
+        private int noCuisinier;
+        public int NoCuisinier
+        {
+            get { return noCuisinier; }
+            set { noCuisinier = value; }
+        }
+
+        private bool diponible;
+        public bool Disponible
+        {
+            get { return diponible; }
+            set { diponible = value; }
+        }
+
+        //Constructeur
+        public Cuisinier(int _noCuisinier)
+        {
+            NoCuisinier = _noCuisinier;
+            Disponible = true;
+        }
+
+        //Méthodes
+        
+        
+    }//Fin class Cuisinier
+}
